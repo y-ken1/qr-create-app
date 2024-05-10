031a35707f5fd69740bae7e50a445ff830717939
YAMASHITA Kenichi <k.yamashita.4649@gmail.com>
2024-05-10 16:52:23 +0900

    QRコード生成 新規作成

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { QRCodeCanvas } from "qrcode.react";
+import { useState } from "react";
+import { Box } from "@mui/material";
 
 function App() {
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
+
+  const textStyle = css`
+    font-size: 16px;
+    padding: 5px 2px;
+    width: 340px;
+  `;
+
+  const [url, setUrl] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Box
+        display="flex"
+        flexDirection="column"
+        marginTop="20px"
+        marginLeft="50px"
+        justifyContent="center"
+        alignItems="start"
+        sx={{ width: "100%", height: "100%" }}
+      >
+        <h1>QR Code Generator</h1>
+        <input
+          css={textStyle}
+          type="url"
+          name="url"
+          value={url}
+          placeholder="(url)"
+          onChange={(e) => setUrl(e.target.value)}
+          onFocus={(e) => e.target.select()}
+        />
+        {url ? (
+          <QRCodeCanvas
+            value={url}
+            size="100"
+            style={{
+              marginTop: "20px",
+              padding: "10px",
+              backgroundColor: "#fff",
+            }}
+          />
+        ) : (
+          <p>ここにQRコードが表示されます</p>
+        )}
+      </Box>
+    </ThemeProvider>
   );
 }
 
